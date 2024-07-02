d6da7b7e80a366f1862304248c5c9db037665fc1
Rahul <87246122+rahulsingh34@users.noreply.github.com>
2024-07-02 16:37:53 -0400

    formatting

--- dbt/models/yearly_race_results.sql
+++ dbt/models/yearly_race_results.sql
@@ -1,14 +1,14 @@
 SELECT DISTINCT
-	rc."year"                                       	AS year,
-	rs."raceId"                                     	AS race_id,
+	rc."year"                                           AS year,
+	rs."raceId"                                         AS race_id,
 	rc."name"                                           AS circuit,
-	cs."name"    			  						    AS constructor,
+	cs."name"    			  	            AS constructor,
 	TRIM(dr."forename") || ' ' || TRIM(dr."surname")    AS driver,
 	rs."positionOrder"                                  AS position,
-	rs."points"                                    	 	AS points,
-	st."status"                                     	AS status
+	rs."points"                                    	    AS points,
+	st."status"                                         AS status
 FROM 
-	results            	       AS rs
+	results            	   AS rs
 	INNER JOIN races    	   AS rc ON rs."raceId" = rc."raceId"
 	INNER JOIN status   	   AS st ON rs."statusId" = st."statusId" 
 	INNER JOIN constructors    AS cs ON rs."constructorId" = cs."constructorId" 
